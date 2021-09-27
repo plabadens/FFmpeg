134c2e8f5960d824faef4a29dc09b89cc25eeec8
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-09-27 11:06:11 +0800

    HACK: libavcodec & rkmppdec: Add FF_CODEC_CAP_CONTIGUOUS_BUFFERS
    Add FF_CODEC_CAP_CONTIGUOUS_BUFFERS to alloc contiguous buffers.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libavcodec/decode.c
+++ libavcodec/decode.c
@@ -1505,6 +1505,14 @@ static int update_frame_pool(AVCodecContext *avctx, AVFrame *frame)
         for (i = 0; i < 4; i++) {
             av_buffer_pool_uninit(&pool->pools[i]);
             pool->linesize[i] = linesize[i];
+
+            if (avctx->codec->caps_internal & FF_CODEC_CAP_CONTIGUOUS_BUFFERS) {
+                if (!i)
+                    size[0] += size[1] + size[2] + size[3];
+                else
+                    continue;
+            }
+
             if (size[i]) {
                 pool->pools[i] = av_buffer_pool_init(size[i] + 16 + STRIDE_ALIGN - 1,
                                                      CONFIG_MEMORY_POISONING ?
@@ -1636,6 +1644,21 @@ static int video_get_buffer(AVCodecContext *s, AVFrame *pic)
 
         pic->data[i] = pic->buf[i]->data;
     }
+
+    if (s->codec->caps_internal & FF_CODEC_CAP_CONTIGUOUS_BUFFERS) {
+        int size;
+
+        for (i = 1; i < 4; i++) {
+            pic->linesize[i] = pool->linesize[i];
+            av_buffer_unref(&pic->buf[i]);
+        }
+
+        size = av_image_fill_pointers(pic->data, pic->format, pic->height,
+                                      pic->buf[0]->data, pic->linesize);
+        if (size < 0 || size > pic->buf[0]->size)
+            goto fail;
+    }
+
     for (; i < AV_NUM_DATA_POINTERS; i++) {
         pic->data[i] = NULL;
         pic->linesize[i] = 0;
